4a151a7c6b813526eb8785f5134365807eef4753
Onur Karakaya <onurkarakaya95.2@gmail.com>
2024-08-16 16:11:13 +0300

    temel entityler olusturuldu.

---
+++ Core/ApiProject.Domain/Entities/Brand.cs
@@ -0,0 +1,24 @@
+﻿using ApiProject.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiProject.Domain.Entities
+{
+	public class Brand : EntityBase
+	{
+        public Brand()
+        {              
+
+        }
+
+        public Brand(string name)
+        {
+            Name = name;
+        }
+
+        public required string Name { get; set; }
+    }
+}
